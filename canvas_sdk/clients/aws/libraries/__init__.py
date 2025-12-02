130fadab1deca842b3bb590e3d9fc474363613bd
dbajet <dbajet@gmail.com>
2025-12-02 23:25:03 +0000

    feat(sdk): add missing exports in canvas_sdk/clients/aws

---
+++ canvas_sdk/clients/aws/libraries/__init__.py
@@ -0,0 +1,3 @@
+from canvas_sdk.clients.aws.libraries.aws_s3 import AwsS3
+
+__all__ = __exports__ = ("AwsS3",)
---
+++ canvas_sdk/clients/aws/structures/__init__.py
@@ -0,0 +1,7 @@
+from canvas_sdk.clients.aws.structures.aws_credentials import AwsCredentials
+from canvas_sdk.clients.aws.structures.aws_s3_item import AwsS3Item
+
+__all__ = __exports__ = (
+    "AwsCredentials",
+    "AwsS3Item",
+)
